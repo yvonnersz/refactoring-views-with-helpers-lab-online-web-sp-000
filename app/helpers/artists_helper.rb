22c0221e05f103dd09315dcc4b86c5c132759237
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-23 08:11:10 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,3 @@
 module ArtistsHelper
-  def artist_exist?
-    if @song.artist_name.present?
-      <%= link_to @song.artist_name, artist_path(@song.artist) %>
-    else
-      <%= link_to "Add Artist", edit_song_path(@song) %>
-    end
-  end
+
 end
